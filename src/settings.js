80579225f5603505fe8017adcaa91e8f80322228
Mark Pavlovski <markpavlovski@gmail.com>
2018-07-13 20:56:55 -0700

    set up heroku server referenve

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 export const INITIAL_PAGE = 1
 export const START_THRESHOLD = 30000
 // export const BACKEND_SERVER = 'http://10.0.0.85:3000'
-export const BACKEND_SERVER = 'http://10.5.82.230:3000'
-// export const BACKEND_SERVER = 'https://adventure-run-backend.herokuapp.com'
+// export const BACKEND_SERVER = 'http://10.5.82.230:3000'
+export const BACKEND_SERVER = 'https://adventure-run-backend.herokuapp.com'
 export const CHECKPOINT_COLLISION_RADIUS = 15
